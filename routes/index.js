16c21fbd543d2fe5043d69a281d279f00cfaea4e
pcatalina <catalina.pavlov@etu.unistra.fr>
2015-04-01 15:35:49 +0200

    set up views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signin', function(req, res, next) {
+    res.render('signin', { title: 'Express' });
+});
+
+router.get('/signup', function(req, res, next) {
+    res.render('signup', { title: 'Express' });
+});
+
+router.get('/tweets', function(req, res, next) {
+    res.render('tweets', { title: 'Express' });
+});
+
+router.get('/myprofile', function(req, res, next) {
+    res.render('myprofile', { title: 'Express' });
+});
+
 module.exports = router;
