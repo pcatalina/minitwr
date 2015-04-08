b629ffb0ba8fbc7f508d4a063b256b98e579ad03
pcatalina <catalina.pavlov@etu.unistra.fr>
2015-04-08 15:13:38 +0200

    change style of the main page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var tweet = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,6 +21,11 @@ router.get('/tweets', function(req, res, next) {
 
 router.get('/myprofile', function(req, res, next) {
     res.render('myprofile', { title: 'Express' });
+});
+
+router.post('/tweets', function(req, res, next) {
+    tweets.unshift(req.body.tweet);
+    res.redirect('/tweets')
 });
 
 module.exports = router;
