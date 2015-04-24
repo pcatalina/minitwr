08d125189f62e77838413dfbc36a1caefd16323b
pcatalina <catalina.pavlov@etu.unistra.fr>
2015-04-25 01:38:02 +0200

    Add POST function

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var tweet = [];
+
+var tweets = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express'});
+});
+
+router.get('/tweets', function(req, res, next) {
+    res.render('tweets', { title: 'Express', tweets: tweets  });
+});
+
+router.post('/tweets', function(req, res, next) {
+    tweets.unshift(req.body.tweet);
+    res.redirect('/tweets')
 });
 
 router.get('/signin', function(req, res, next) {
@@ -15,17 +25,8 @@ router.get('/signup', function(req, res, next) {
     res.render('signup', { title: 'Express' });
 });
 
-router.get('/tweets', function(req, res, next) {
-    res.render('tweets', { title: 'Express' });
-});
-
 router.get('/myprofile', function(req, res, next) {
     res.render('myprofile', { title: 'Express' });
-});
-
-router.post('/tweets', function(req, res, next) {
-    tweets.unshift(req.body.tweet);
-    res.redirect('/tweets')
 });
 
 module.exports = router;
