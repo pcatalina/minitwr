8f6a5e31ec0f032612d2619be36c6b1a92c35bf7
ivan.aksamentov <ivan.aksamentov@gmail.com>
2015-04-25 20:46:40 +0200

    make `env` `development` by default

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
