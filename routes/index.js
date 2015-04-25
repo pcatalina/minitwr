6dbe7d26856f1409d4e616b1729a357e2e58d65b
ivan.aksamentov <ivan.aksamentov@gmail.com>
2015-04-25 05:55:31 +0200

    format code

--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,29 @@ var router = express.Router();
 var tweets = [];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index');
+router.get('/', function (req, res, next) {
+  res.render('index');
 });
 
-router.get('/tweets', function(req, res, next) {
-    res.render('tweets', { tweets: tweets  });
+router.get('/tweets', function (req, res, next) {
+  res.render('tweets', {tweets: tweets});
 });
 
-router.post('/tweets', function(req, res, next) {
-    tweets.unshift(req.body.tweet);
-    res.redirect('/tweets')
+router.post('/tweets', function (req, res, next) {
+  tweets.unshift(req.body.tweet);
+  res.redirect('/tweets')
 });
 
-router.get('/signin', function(req, res, next) {
-    res.render('signin');
+router.get('/signin', function (req, res, next) {
+  res.render('signin');
 });
 
-router.get('/signup', function(req, res, next) {
-    res.render('signup');
+router.get('/signup', function (req, res, next) {
+  res.render('signup');
 });
 
-router.get('/myprofile', function(req, res, next) {
-    res.render('myprofile');
+router.get('/myprofile', function (req, res, next) {
+  res.render('myprofile');
 });
 
 module.exports = router;
