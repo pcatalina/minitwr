c4083091012c3c512fd996def1f34aa8bd910c2b
ivan.aksamentov <ivan.aksamentov@gmail.com>
2015-04-25 06:48:07 +0200

    refactor dir structure

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', routes);
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var tweetService = require('../app/tweetService');
+var tweetService = require('../services/tweetService');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
