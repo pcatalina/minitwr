108938448496cf1be7a6b6db82308073687e0cb0
ivan.aksamentov <ivan.aksamentov@gmail.com>
2015-05-09 15:32:47 +0200

    add tweet posting example

--- public/scripts/tweets.js
+++ public/scripts/tweets.js
@@ -1,21 +1,20 @@
-$(document).ready(function()
-{
-    var dates = $("p#date");
-    dates.each(function(i) {
-        var date = $(this).text();
-        $(this).text(moment(date).fromNow());
+$(document).ready(function() {
+  var dates = $("p#date");
+  dates.each(function(i) {
+    var date = $(this).text();
+    $(this).text(moment(date).fromNow());
 
-    });
+  });
 
-    var messages = $("li#message");
-    messages.each(function (j) {
-        $(messages[j + 1]).insertBefore(messages[j]);
-        var message = $(this).text();
-        console.log(message);
-    });
+  var messages = $("li#message");
+  messages.each(function(j) {
+    $(messages[j + 1]).insertBefore(messages[j]);
+    var message = $(this).text();
+    console.log(message);
+  });
 
   /*
-   A test example of API consumption
+   A test example of API consumption: tweets retrieval
    TODO: get rid of
    */
   $.get('/api/tweets')
@@ -24,6 +23,23 @@ $(document).ready(function()
     })
     .fail(function(err) {
       console.log(err);
+    });
+
+
+  /*
+   A test example of API consumption: tweet posting
+   TODO: get rid of
+   */
+  var newTweet = {
+    tweetText: "Blah blah blah"
+  };
+
+  $.post('/api/tweets', newTweet)
+    .done(function(res) {
+      console.log(res);
+    })
+    .fail(function(res) {
+      console.log(res);
     });
 
 });
