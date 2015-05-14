4b0e5d2a207d36c36ab7f1ec22fa03b8c43fd8c2
pcatalina <catalina.pavlov@etu.unistra.fr>
2015-05-14 03:22:37 +0200

    right validation of signup form

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,76 +1,89 @@
-
-$("input#password").mouseover(function(){
+$("input#password").mouseover(function () {
     $('#password').popover('show');
 });
 
-$("input#password").mouseout(function(){
+$("input#password").mouseout(function () {
     $('#password').popover('hide');
 });
 
-var passWord = document.getElementById("password").innerHTML;
-
-function password(passWord) {
-  var i = 0;
-  var accepted = false;
-  if(passWord.length >= 8) {
-    for(i; i < passWord.length; i++)
-      if((passWord[i] >= 'A') && (passWord[i] <= 'Z'))
-        for(i; i < passWord.length; i++)
-          if((passWord[i] >= 'a') && (passWord[i] <= 'z'))
-            for(i; i < passWord.length; i++)
-              if((passWord[i] >= 33) && (passWord[i] <= 38))
-                accepted = true;
-  }
-  return accepted;
+function reset($elem) {
+    $elem.before($elem.clone(true));
+    var $newElem = $elem.prev();
+    $elem.remove();
+    return $newElem;
 }
 
-$(document).ready(function(){
-    $("button").click(function(){
-        if ((password(passWord))== true)
-        {
-            $.post("/api/users",{
+function validPassword(password) {
+    return ((password == '') || (password.length < 8))
+}
+
+function validUserName(username) {
+    return (username == null || username.length < 3 )
+}
+
+function validEmail(email) {
+    var emailForm = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return (!emailForm.test(email))
+}
+$(document).ready(function () {
+
+    $('button#buttonSignUp').click(function () {
+
+        var username = $('input#username').val();
+        console.log(username);
+        var email = $('input#email').val();
+        console.log(email);
+        var password = $('input#password').val();
+        console.log(password);
+
+        if (validPassword(password) || validUserName(username) || validEmail(email)) {
+            if (validPassword(password)) {
+                var pswd = $('input#password');
+                pswd.removeClass();
+                pswd = reset(pswd);
+                pswd.addClass("body form-control registerBlanck shake animated");
+            }
+            if (validUserName(username)) {
+                var usnm = $('input#username');
+                usnm.removeClass();
+                usnm = reset(usnm);
+                usnm.addClass("body form-control registerBlanck shake animated");
+            }
+            if (validEmail(email)) {
+                var mail = $('input#email');
+                mail.removeClass();
+                mail = reset(mail);
+                mail.addClass("body form-control registerBlanck shake animated");
+            }
+
+        }
+        else {
+
+            $.post("/api/users", {
                 username: username,
                 email: email,
                 password: password
-            });
+            })
+                .done(function (res) {
+                    window.location.replace("/signin");
+                })
+                .fail(function (res) {
+                    console.log(res);
+                })
+
         }
-        else
-        {
-            $("#password").popover({ title: 'Look! A bird!', content: 'ewfew' });
-        }
-        console.log(passWord);
-        });
+    });
 });
 
 
-
-/*var fullName = document.getElementById("exampleInputFullName").innerHTML;
-
-var eMail = document.getElementById("exampleInputEmail1").innerHTML;
-//var signUp = false;
-
-function validateFullName() {
-  var x = document.getElementById("exampleInputFullName").innerHTML;
-  if(x == null || x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-
-}
-
-function validateEmail(eMail) {
-  if(eMail = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i);
-}
+/*
 
 
 
-function signUp() {
-  if((validateFullName()) && (validateEmail(eMail)) && (password(passWord))) {
-    window.location.href = 'myprofile';
-  }
-}
+ function signUp() {
+ if((validateFullName()) && (validateEmail(eMail)) && (password(passWord))) {
+ window.location.href = 'myprofile';
+ }
+ }
 
-
-/**
- * Created by C on 28.04.2015.
  */
