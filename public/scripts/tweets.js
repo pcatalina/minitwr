688d20fdefaf339095659c3ba035231feafe1cfa
ivan.aksamentov <ivan.aksamentov@gmail.com>
2015-05-17 14:37:19 +0200

    format

--- public/scripts/tweets.js
+++ public/scripts/tweets.js
@@ -1,54 +1,54 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    $("input#button").click(function () {
+  $("input#button").click(function() {
 
-        var text = $('textarea#text').first().val();
-        console.log(text);
-        if (text === "") {
-            $("p#alert").text("Write something !!!");
-        }
-        else {
-            $.post('/api/tweets', {
-                tweetText: text
-            })
-                .done(function (res) {
+    var text = $('textarea#text').first().val();
+    console.log(text);
+    if(text === "") {
+      $("p#alert").text("Write something !!!");
+    }
+    else {
+      $.post('/api/tweets', {
+        tweetText: text
+      })
+        .done(function(res) {
 
-                    appendTweet(function (date, user, message) {
-                        date.text(res.date).text(moment(res.date).fromNow());
-                        user.append(res.user.username);
-                        message.text(res.text);
+          appendTweet(function(date, user, message) {
+            date.text(res.date).text(moment(res.date).fromNow());
+            user.append(res.user.username);
+            message.text(res.text);
 
-                    });
+          });
 
-                    $('textarea#text').val('');
-                })
-                .fail(function (res) {
-                    console.log(res);
-                });
+          $('textarea#text').val('');
+        })
+        .fail(function(res) {
+          console.log(res);
+        });
 
-        }
+    }
 
-    });
+  });
 });
 
 function appendTweet(done) {
-    var ul = $("ul#tweet-list");
-    var listItem = $("<li>").addClass("bounceInRight animated tweets media");
-    ul.prepend(listItem);
+  var ul = $("ul#tweet-list");
+  var listItem = $("<li>").addClass("bounceInRight animated tweets media");
+  ul.prepend(listItem);
 
-    var date = $("<p>").addClass("date");
+  var date = $("<p>").addClass("date");
 
-    listItem.append(date);
+  listItem.append(date);
 
-    var user = $("<p>").addClass("media-heading user");
-    var img = $("<img>").addClass("img pull-left media-object")
-                        .attr('src', '../images/icon.png');
-    user.append(img);
+  var user = $("<p>").addClass("media-heading user");
+  var img = $("<img>").addClass("img pull-left media-object")
+    .attr('src', '../images/icon.png');
+  user.append(img);
 
-    listItem.append(user);
+  listItem.append(user);
 
-    var message = $("<p>").addClass("message");
+  var message = $("<p>").addClass("message");
 
-    listItem.append(message);
-    done(date, user, message);
+  listItem.append(message);
+  done(date, user, message);
 }
