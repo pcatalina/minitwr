c3a0670d417268701936ccd69d6b461301493ee2
pcatalina <catalina.pavlov@etu.unistra.fr>
2015-05-20 12:51:30 +0200

    load tweets dynamically

--- public/scripts/tweets.js
+++ public/scripts/tweets.js
@@ -1,45 +1,57 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    $("input#button").click(function () {
-
-        var text = $('textarea#text').first().val();
-        console.log(text);
-        if (text === "") {
-            $("p#alert").text("Write something !!!");
-        }
-        else {
-            $("p#alert").text("");
-            $.post('/api/tweets', {
-                tweetText: text
-            })
-                .done(function (res) {
-                    var date = moment(res.date).fromNow(),
-                        username = res.user.username,
-                        message = res.text;
-
-                    appendTweet(date, username, message);
-
-                    $('textarea#text').val('');
-                })
-                .fail(function (res) {
-                    console.log(res);
-                });
-        }
+  $.get('/api/tweets')
+    .done(function(tweets) {
+      $.each(tweets, function(i, tweet) {
+        var date = moment(tweet.date).fromNow(),
+          username = tweet.user.username,
+          message = tweet.text;
+        appendTweet(date, username, message);
+      });
+    })
+    .fail(function(res) {
+      console.log(res);
     });
+
+  $("input#button").click(function() {
+
+    var text = $('textarea#text').first().val();
+    console.log(text);
+    if(text === "") {
+      $("p#alert").text("Write something !!!");
+    }
+    else {
+      $.post('/api/tweets', {
+        tweetText: text
+      })
+        .done(function(res) {
+          var date = moment(res.date).fromNow(),
+            username = res.user.username,
+            message = res.text;
+
+          appendTweet(date, username, message);
+
+          $('textarea#text').val('');
+        })
+        .fail(function(res) {
+          console.log(res);
+        });
+    }
+  });
 });
 
 function appendTweet(date, username, message) {
-    $.get('/partials/tweet')  // get partial html from server
-        .done(function (res) {
-            var tweet = $(res);   // create DOM element from it
-            $("ul#tweet-list").prepend(tweet);  // prepend to list
+  $.get('/partials/tweet')  // get partial html from server
+    .done(function(res) {
+      var tweet = $(res);   // create DOM element from it
+      $("ul#tweet-list").prepend(tweet);  // prepend to list
 
-            // find text elements and put text
-            tweet.find('p#date').text(date);
-            tweet.find('p#username').text(username);
-            tweet.find('p.message').text(message);
-        })
-        .fail(function (res) {
-            console.log(res);
-        });
+      // find text elements and put text
+      tweet.find('p#date').text(date);
+      tweet.find('p#username').text(username);
+      tweet.find('p.message').text(message);
+    })
+    .fail(function(res) {
+      console.log(res);
+    });
 }
